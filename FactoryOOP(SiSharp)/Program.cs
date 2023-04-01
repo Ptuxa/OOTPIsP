9ad4f293f6166a92c99aa5f245c264c0f5e06d59
nstandm@gmail.com <nstandm@gmail.com>
2023-04-01 23:11:22 +0300

    Добавьте файлы проекта.

---
+++ FactoryOOP(SiSharp)/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactoryOOP_SiSharp_
+{
+    internal static class Program
+    {
+        class ProcessorInfo
+        {
+            public string CPUMicroarchitectur;
+            public int clockFrequency;
+        }
+
+        class InteractionInterfaceGeneral
+        {
+            public string bluetooth;
+            public bool NFC;
+        }
+
+        abstract class InteractionInterfacePC : InteractionInterfaceGeneral
+        {
+            public int maximumUSBTransferRate;
+            public ProcessorInfo processor;
+        }
+
+        abstract class InteractionInterfaceMobile : InteractionInterfaceGeneral
+        {
+            public bool frontLightning;
+            public ProcessorInfo processor;
+        }
+
+        class DesktopPC : InteractionInterfacePC
+        {
+            public bool monoblock;
+        }
+
+        class Laptop : InteractionInterfacePC
+        {
+            public int batteryAutonomy;
+        }
+
+        class Smartphone : InteractionInterfaceMobile
+        {
+            public string audioTypeSupportLinks;
+            public string connectionConnector;
+        }
+
+        class ElectronicWatch : InteractionInterfaceMobile
+        {
+            public bool ECGSensor, bloodPressureMeasurement;
+        }
+    }
+}
---
+++ FactoryOOP(SiSharp)/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("FactoryOOP(SiSharp)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FactoryOOP(SiSharp)")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("6ce8c9ed-f589-43ed-ab6b-a47a312f8f78")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
